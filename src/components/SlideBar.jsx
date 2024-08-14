eab4b834d8d0e6c30cfddbba8fc40c727de14679
Martynas-J <Aurimhas@gmail.com>
2024-08-14 19:32:34 +0300

    add mobil no arrow

--- src/components/SlideBar.jsx
+++ src/components/SlideBar.jsx
@@ -12,17 +12,17 @@ const SlideBar = ({ weapons, selectedWeaponIndex, handleCardClick }) => {
     scrollContainerRef.current.scrollBy({ left: 200, behavior: "smooth" });
   };
   const buttonClass =
-    "absolute top-1/2  z-50 transform -translate-y-1/2 px-3  text-green-950 text-8xl rounded-full  focus:outline-none";
+    "absolute top-1/2  z-50 transform -translate-y-1/2 px-1  text-green-900 text-4xl rounded-full bg-gray-800/50 focus:outline-none";
   if (!weapons || !weapons.length) {
     return;
   }
   return (
     <div className="relative w-full ">
-      <button onClick={scrollLeft} className={`${buttonClass} left-0`}>
-        🢐 {/* Left arrow */}
+      <button onClick={scrollLeft} className={`${buttonClass} left-0 hidden sm:block`}>
+      &#11160; {/* Left arrow */}
       </button>
-      <button onClick={scrollRight} className={`${buttonClass} right-0`}>
-        🢒 {/* Right arrow */}
+      <button onClick={scrollRight} className={`${buttonClass} right-0 hidden sm:block`}>
+        &#11162; {/* Right arrow */}
       </button>
       <div
         ref={scrollContainerRef}
